c4e52276244e48b59d21c11566897cd358f84042
kaz-second-branded <nikensailendra6@gmail.com>
2026-02-08 15:07:47 +0700

    Create admin.js

---
+++ admin.js
@@ -0,0 +1,53 @@
+const USER = "admin";
+const PASS = "12345";
+
+function login(){
+  const u = document.getElementById("username").value;
+  const p = document.getElementById("password").value;
+
+  if(u === USER && p === PASS){
+    document.getElementById("loginBox").style.display = "none";
+    document.getElementById("dashboard").style.display = "block";
+    loadProducts();
+  }else{
+    alert("Username atau password salah");
+  }
+}
+
+function addProduct(){
+  const name = document.getElementById("pname").value;
+  const price = document.getElementById("pprice").value;
+  const stock = document.getElementById("pstock").value;
+  const image = document.getElementById("pimage").value;
+
+  db.ref("products").push({ name, price, stock, image });
+  alert("Produk ditambahkan");
+}
+
+function loadProducts(){
+  const list = document.getElementById("adminList");
+
+  db.ref("products").on("value", snapshot => {
+    list.innerHTML = "";
+    snapshot.forEach(item => {
+      const p = item.val();
+      const key = item.key;
+
+      const div = document.createElement("div");
+      div.className = "card";
+
+      div.innerHTML = `
+        <img src="${p.image}">
+        <h3>${p.name}</h3>
+        <p>Stok: ${p.stock}</p>
+        <button onclick="deleteProduct('${key}')">Hapus</button>
+      `;
+
+      list.appendChild(div);
+    });
+  });
+}
+
+function deleteProduct(id){
+  db.ref("products/" + id).remove();
+}
