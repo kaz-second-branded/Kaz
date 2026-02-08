721f72c0e27ce1bcd85a3d8db0f2d09078dc319f
kaz-second-branded <nikensailendra6@gmail.com>
2026-02-08 13:10:51 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,3 +1,4 @@
+<!-- Firebase SDKs -->
 <script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js"></script>
 <script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-auth-compat.js"></script>
 <script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore-compat.js"></script>
@@ -5,16 +6,18 @@
 
 <script>
 const firebaseConfig = {
-  apiKey: "ISI_API_KEY",
-  authDomain: "PROJECT.firebaseapp.com",
-  projectId: "PROJECT",
-  storageBucket: "PROJECT.appspot.com",
-  messagingSenderId: "ID",
-  appId: "APP_ID"
+  apiKey: "AIzaSyA42lKOIGWKgVQtW7Uu3W5rqv1cQr9r3zE",
+  authDomain: "kazz-second-branded.firebaseapp.com",
+  projectId: "kazz-second-branded",
+  storageBucket: "kazz-second-branded.appspot.com",
+  messagingSenderId: "831976619012",
+  appId: "1:831976619012:web:d54479e50e3d245156c951"
 };
 
+// Init Firebase
 firebase.initializeApp(firebaseConfig);
 
+// Global services
 const auth = firebase.auth();
 const db = firebase.firestore();
 const storage = firebase.storage();
