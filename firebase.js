ceb3ee4667b483542c73c65befaf92497e936a69
kaz-second-branded <nikensailendra6@gmail.com>
2026-02-08 14:56:34 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,18 +1,17 @@
 <script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-auth-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore-compat.js"></script>
+<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-database-compat.js"></script>
 
 <script>
-const firebaseConfig = {
-  apiKey: "AIzaSyA42lKOIGWKgVQtW7Uu3W5rqv1cQr9r3zE",
-  authDomain: "kazz-second-branded.firebaseapp.com",
-  projectId: "kazz-second-branded",
-  messagingSenderId: "831976619012",
-  appId: "1:831976619012:web:d54479e50e3d245156c951"
-};
+  const firebaseConfig = {
+    apiKey: "AIzaSyA42lKOIGWKgVQtW7Uu3W5rqv1cQr9r3zE",
+    authDomain: "kazz-second-branded.firebaseapp.com",
+    databaseURL: "https://kazz-second-branded-default-rtdb.firebaseio.com",
+    projectId: "kazz-second-branded",
+    storageBucket: "kazz-second-branded.appspot.com",
+    messagingSenderId: "831976619012",
+    appId: "1:831976619012:web:d54479e50e3d245156c951"
+  };
 
-firebase.initializeApp(firebaseConfig);
-
-const auth = firebase.auth();
-const db = firebase.firestore();
+  firebase.initializeApp(firebaseConfig);
+  const db = firebase.database();
 </script>
